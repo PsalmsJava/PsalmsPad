ad62a181bd85ff6f9230ead68b56aa507bf64441
PsalmsJava <inyangpsalms@gmail.com>
2021-04-30 12:24:34 +0100

    Created a Note Taking App In Android Studio

---
+++ app/src/main/java/com/psalms/psalmspad/NotesAdapter.java
@@ -0,0 +1,64 @@
+package com.psalms.psalmspad;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotesAdapter extends RecyclerView.Adapter <NotesAdapter.MyViewHolder> {
+
+    private List <NotesBuilder>notesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, content;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            content = (TextView) view.findViewById(R.id.content);
+
+        }
+    }
+
+    public NotesAdapter(List <NotesBuilder> notesList) {
+        this.notesList = notesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        NotesBuilder note = notesList.get(position);
+        holder.title.setText(note.getTitle());
+        holder.content.setText(note.getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+}
---
+++ app/src/main/java/com/psalms/psalmspad/NotesBuilder.java
@@ -0,0 +1,23 @@
+package com.psalms.psalmspad;
+
+public class NotesBuilder {
+
+    private String title,
+            content;
+
+    public NotesBuilder() {
+    }
+
+    public NotesBuilder(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
